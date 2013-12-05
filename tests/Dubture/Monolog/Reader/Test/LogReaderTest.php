a33890ccefab0fc6c3c0446d25ee269e45ab4f17
jelte <jelte@marlon.be>
2013-12-05 09:28:05 +0100

    Test LogReader independant of LineLogParser

--- tests/Dubture/Monolog/Reader/Test/LogReaderTest.php
+++ tests/Dubture/Monolog/Reader/Test/LogReaderTest.php
@@ -11,7 +11,6 @@
 
 namespace Dubture\Monolog\Reader\Test;
 
-use Dubture\Monolog\Reader\ReverseLogReader;
 use Dubture\Monolog\Reader\LogReader;
 
 
@@ -23,44 +22,35 @@ class LogReaderTest extends \PHPUnit_Framework_TestCase
     public function testReader()
     {
         $file = __DIR__ . '/../../../../files/test.log';
-        $reader = new LogReader($file);
+        $logParser = $this->getMock('Dubture\Monolog\Parser\LogParserInterface', array(), array(), '', false);
+        $logParser->expects($this->exactly(2))->method('parse')->will($this->returnValue(array()));
+        $reader = new LogReader($file, $logParser);
 
         $log = $reader[0];
-
-        $this->assertInstanceOf('\DateTime', $log['date']);
-        $this->assertEquals('test', $log['logger']);
-        $this->assertEquals('INFO', $log['level']);
-        $this->assertEquals('foobar', $log['message']);
-        $this->assertArrayHasKey('foo', $log['context']);
+        $this->assertInternalType('array', $log);
 
         $log = $reader[1];
-
-        $this->assertInstanceOf('\DateTime', $log['date']);
-        $this->assertEquals('aha', $log['logger']);
-        $this->assertEquals('DEBUG', $log['level']);
-        $this->assertEquals('foobar', $log['message']);
-        $this->assertArrayNotHasKey('foo', $log['context']);
-
+        $this->assertInternalType('array', $log);
     }
 
     public function testIterator()
     {
         $file = __DIR__ . '/../../../../files/test.log';
-        $reader = new LogReader($file);
+        $logParser = $this->getMock('Dubture\Monolog\Parser\LogParserInterface', array(), array(), '', false);
+        $logParser->expects($this->exactly(2))->method('parse')->will($this->returnValue(array()));
+        $reader = new LogReader($file, $logParser);
         $lines = array();
         $keys = array();
 
         $this->assertEquals(2, count($reader));
 
         foreach ($reader as $i => $log) {
-            $test = $reader[0];
             $lines[] = $log;
             $keys[] = $i;
         }
 
         $this->assertEquals(array(0, 1), $keys);
-        $this->assertEquals('test', $lines[0]['logger']);
-        $this->assertEquals('aha', $lines[1]['logger']);
+        $this->assertCount(2, $lines);
 
     }
 
@@ -70,7 +60,8 @@ class LogReaderTest extends \PHPUnit_Framework_TestCase
     public function testException()
     {
         $file = __DIR__ . '/../../../../files/test.log';
-        $reader = new LogReader($file);
+        $logParser = $this->getMock('Dubture\Monolog\Parser\LogParserInterface', array(), array(), '', false);
+        $reader = new LogReader($file, $logParser);
         $reader[5] = 'foo';
 
     }
